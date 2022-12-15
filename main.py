9efdd0182b86368c994c077fd6996256a19aa08d
Anna Ter <annateranna@gmail.com>
2022-12-15 15:39:50 +0200

    unittests

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import files_in_file
+import reverse_2_times
+import unittest
+
+
+class TestReverse(unittest.TestCase):
+    def test_a_bigger_than_y(self):
+        self.assertEqual(reverse_2_times.reverse_2_times("9 2 5 6 9"), "1 5 4 3 2 9 8 7 6")
+
+    def test_a_smaller_than_y(self):
+        self.assertEqual(reverse_2_times.reverse_2_times("9 3 6 5 8"), "1 2 6 5 8 7 3 4 9")
+
+
 if __name__ == '__main__':
-    files_in_file
+    unittest.main()
 
--- reverse_2_times.py
+++ reverse_2_times.py
@@ -1,5 +1,7 @@
-n, x, y, a, b = input().split()
-seq = [i for i in range(1, int(n) + 1)]
-seq[int(x) - 1:int(y)] = seq[int(x) - 1:int(y)][::-1]
-seq[int(a) - 1:int(b)] = seq[int(a) - 1:int(b)][::-1]
-print(" ".join(map(str, seq)))
+def reverse_2_times(s):
+    n, x, y, a, b = s.split()
+    seq = [i for i in range(1, int(n) + 1)]
+    seq[int(x) - 1:int(y)] = seq[int(x) - 1:int(y)][::-1]
+    seq[int(a) - 1:int(b)] = seq[int(a) - 1:int(b)][::-1]
+    return " ".join(map(str, seq))
+#    print(" ".join(map(str, seq)))
